7afe8dd66439bfa704281ff1676a7826b6bfd756
Denis Savenok <denissavenok@gmail.com>
2015-06-29 00:05:32 +0400

    onSectionTitleClick as event handler; accordion css

--- client/coffee/components/list/contributorItemView.coffee
+++ client/coffee/components/list/contributorItemView.coffee
@@ -1,9 +1,38 @@
 define [
+    "jquery"
+    "underscore"
     "marionette"
     "hbs!components/list/contributor"
-], (Marionette, contributorTemplate) ->
+], ($, _, Marionette, contributorTemplate) ->
 
     ContributorItemView = Marionette.ItemView.extend
         tagName: "div"
         className: "accordion-section"
         template: contributorTemplate
+
+        events:
+            "click .accordion-section-title" : "onSectionTitleClick"
+
+        onSectionTitleClick: (event) ->
+            openSection = (selector) ->
+                $(selector).slideDown(300).addClass('open')
+
+            closeSection = ->
+                $('.accordion .accordion-section-title').removeClass('active')
+                $('.accordion .accordion-section-content')
+                    .slideUp(300)
+                    .removeClass('open')
+
+            currentAttrValue = $(event.target).attr('href')
+
+            author = _.last currentAttrValue.split("/")
+
+            if $(event.target).is('.active')
+                closeSection()
+            else 
+                closeSection()
+
+                $(event.target).addClass('active')
+                openSection("##{author}")
+
+            event.preventDefault()
--- client/js/components/list/contributorItemView.js
+++ client/js/components/list/contributorItemView.js
@@ -1,8 +1,31 @@
-define(["marionette", "hbs!components/list/contributor"], function(Marionette, contributorTemplate) {
+define(["jquery", "underscore", "marionette", "hbs!components/list/contributor"], function($, _, Marionette, contributorTemplate) {
   var ContributorItemView;
   return ContributorItemView = Marionette.ItemView.extend({
     tagName: "div",
     className: "accordion-section",
-    template: contributorTemplate
+    template: contributorTemplate,
+    events: {
+      "click .accordion-section-title": "onSectionTitleClick"
+    },
+    onSectionTitleClick: function(event) {
+      var author, closeSection, currentAttrValue, openSection;
+      openSection = function(selector) {
+        return $(selector).slideDown(300).addClass('open');
+      };
+      closeSection = function() {
+        $('.accordion .accordion-section-title').removeClass('active');
+        return $('.accordion .accordion-section-content').slideUp(300).removeClass('open');
+      };
+      currentAttrValue = $(event.target).attr('href');
+      author = _.last(currentAttrValue.split("/"));
+      if ($(event.target).is('.active')) {
+        closeSection();
+      } else {
+        closeSection();
+        $(event.target).addClass('active');
+        openSection("#" + author);
+      }
+      return event.preventDefault();
+    }
   });
 });
